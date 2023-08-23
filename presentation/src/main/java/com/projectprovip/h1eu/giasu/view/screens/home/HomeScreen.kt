e627496d3f41245bf4ed85f475403ec6394ee02b
H1eu69 <anrayno200@gmail.com>
2023-08-23 10:17:04 +0700

    add subject category item

--- presentation/src/main/java/com/projectprovip/h1eu/giasu/view/screens/home/HomeScreen.kt
+++ presentation/src/main/java/com/projectprovip/h1eu/giasu/view/screens/home/HomeScreen.kt
@@ -1,11 +1,20 @@
 package com.projectprovip.h1eu.giasu.view.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
@@ -20,9 +29,14 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -31,14 +45,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.projectprovip.h1eu.giasu.R
+import com.projectprovip.h1eu.giasu.ui.composes.SubjectCategoryItem
 import com.projectprovip.h1eu.giasu.ui.theme.primaryColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun HomeScreen() {
+    val searchTextField = remember {
+        mutableStateOf("")
+    }
     Scaffold(
-        containerColor = primaryColor,
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.largeTopAppBarColors(
@@ -55,7 +72,7 @@ fun HomeScreen() {
                             )
                         )
                         Text(
-                            text = "{Usernamfdfde}",
+                            text = "{Username}",
                             style = TextStyle(
                                 fontWeight = FontWeight.Bold,
                                 color = Color.White,
@@ -70,11 +87,18 @@ fun HomeScreen() {
     ) {
         Column(
             Modifier
+                .background(primaryColor)
                 .padding(it)
-                .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top) {
-            OutlinedTextField(value = "",
+                .fillMaxSize() ,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(),
+                value = searchTextField.value,
+                singleLine = true,
                 leadingIcon = {
                     Icon(Icons.Default.Search, "")
                 },
@@ -84,10 +108,83 @@ fun HomeScreen() {
                 shape = RoundedCornerShape(30),
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     containerColor = Color.White,
+                    focusedLabelColor = Color.Blue,
+                    focusedBorderColor = Color.Blue,
+                    focusedLeadingIconColor = Color.Blue
                 ),
                 onValueChange = {
-
-            })
+                    searchTextField.value = it
+                })
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White, RoundedCornerShape(topStart = 50.dp, topEnd = 50.dp))
+            ) {
+                Row(
+                    Modifier
+                        .padding(30.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Category",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily.SansSerif
+                        )
+                    )
+                    Text(
+                        text = "View all",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Normal,
+                            color = Color.DarkGray,
+                            fontSize = 14.sp,
+                            fontFamily = FontFamily.SansSerif
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.height(0.dp))
+                val list = listOf<Int>(1,2,3,4,5,5,5,5,5,55,5,5)
+                LazyRow(
+                    Modifier.fillMaxWidth(),
+                    userScrollEnabled = true,
+                    horizontalArrangement = Arrangement.spacedBy(20.dp),
+                    contentPadding = PaddingValues(start = 20.dp, end = 20.dp),
+                ){
+                    items(list.size) {
+                        SubjectCategoryItem()
+                    }
+                }
+                Row(
+                    Modifier
+                        .padding(30.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Newest classes",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily.SansSerif
+                        )
+                    )
+                    Text(
+                        text = "View all",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Normal,
+                            color = Color.DarkGray,
+                            fontSize = 14.sp,
+                            fontFamily = FontFamily.SansSerif
+                        )
+                    )
+                }
+            }
         }
     }
 }
+
+
