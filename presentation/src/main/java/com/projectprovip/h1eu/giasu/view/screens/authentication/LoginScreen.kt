5a50d085622fcd5677f2df1e4d46b95aa1a1a8df
H1eu69 <anrayno200@gmail.com>
2023-08-24 12:32:09 +0700

    add class item for class management screen

--- presentation/src/main/java/com/projectprovip/h1eu/giasu/view/screens/authentication/LoginScreen.kt
+++ presentation/src/main/java/com/projectprovip/h1eu/giasu/view/screens/authentication/LoginScreen.kt
@@ -11,33 +11,45 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.projectprovip.h1eu.giasu.R
 import com.projectprovip.h1eu.giasu.ui.composes.MainTextField
 import com.projectprovip.h1eu.giasu.ui.theme.primaryColor
+import com.projectprovip.h1eu.giasu.view.navigation.NavControllerProvider
 import com.projectprovip.h1eu.giasu.view.navigation.BottomBarScreens
 import com.projectprovip.h1eu.giasu.view.navigation.Screens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(navController: NavController) {
+fun LoginScreen(@PreviewParameter(NavControllerProvider::class)
+                navController: NavController = rememberNavController()) {
     val fontFamily = FontFamily(
         Font(R.font.mont_bold, FontWeight.Bold),
         Font(R.font.mont_regular, FontWeight.Normal)
@@ -89,7 +101,8 @@ fun LoginScreen(navController: NavController) {
                         .fillMaxWidth()
                         .clickable(
                             interactionSource = interactionSource,
-                            indication = null) {
+                            indication = null
+                        ) {
                             navController.navigate(Screens.Authentication.ForgetPassword.route)
                         },
                     text = "Forget password?",
