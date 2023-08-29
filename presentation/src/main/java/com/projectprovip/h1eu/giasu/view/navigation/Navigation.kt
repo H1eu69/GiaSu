f7fb5777f5bf86692d4fa9820fc13b3167be8b6d
H1eu69 <anrayno200@gmail.com>
2023-08-29 14:00:19 +0700

    add customize bottom bar

--- presentation/src/main/java/com/projectprovip/h1eu/giasu/view/navigation/Navigation.kt
+++ presentation/src/main/java/com/projectprovip/h1eu/giasu/view/navigation/Navigation.kt
@@ -2,20 +2,35 @@
 
 package com.projectprovip.h1eu.giasu.view.navigation
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
@@ -25,6 +40,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import com.projectprovip.h1eu.giasu.ui.theme.primaryColor
 import com.projectprovip.h1eu.giasu.view.screens.authentication.ForgetPasswordScreen
 import com.projectprovip.h1eu.giasu.view.screens.authentication.LoginScreen
 import com.projectprovip.h1eu.giasu.view.screens.authentication.SignUpScreen
@@ -63,7 +79,6 @@ fun NavGraphBuilder.authenticationGraph(navController: NavController) {
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun InAppScreen(navController: NavHostController = rememberNavController()) {
     Scaffold(
@@ -71,15 +86,16 @@ fun InAppScreen(navController: NavHostController = rememberNavController()) {
             BottomBar(navController)
         }
     ) {
-        InAppNavGraph(navController)
+        InAppNavGraph(Modifier.padding(it),navController)
     }
 }
 
 @Composable
-fun InAppNavGraph(navController: NavHostController) {
+fun InAppNavGraph(modifier: Modifier, navController: NavHostController) {
     NavHost(
         navController, startDestination = Screens.InApp.HomeBottomBar.route,
-        route = Screens.InApp.route
+        route = Screens.InApp.route,
+        modifier = modifier,
     ) {
         composable(Screens.InApp.HomeBottomBar.route) { HomeScreen(navController) }
         composable(Screens.InApp.ClassDetail.route) { ClassDetailScreen(navController) }
@@ -100,37 +116,79 @@ fun BottomBar(navController: NavHostController) {
     val bottomBarDestination = screens.any {
         it.route == currentDestination?.route
     }
+    val modifier = Modifier.fillMaxWidth().border(BorderStroke(1.dp, Color.LightGray),
+        shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
     if (bottomBarDestination) {
-        BottomNavigation {
+        Row(
+            modifier = modifier,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ){
             screens.forEach { screen ->
-                BottomNavigationItem(
-                    icon = {
-                        if (screen.route == Screens.InApp.HomeBottomBar.route)
-                            Icon(Icons.Filled.Home, contentDescription = null)
-                        if (screen.route == Screens.InApp.ClassBottomBar.route)
-                            Icon(Icons.Filled.Clear, contentDescription = null)
-                    },
-                    label = { Text(stringResource(screen.resId)) },
-                    selected = currentDestination?.route == screen.route,
-                    selectedContentColor = Color.White,
-                    unselectedContentColor = Color.LightGray,
-                    onClick = {
-                        navController.navigate(screen.route) {
-                            // Pop up to the start destination of the graph to
-                            // avoid building up a large stack of destinations
-                            // on the back stack as users select items
-                            popUpTo(navController.graph.findStartDestination().id) {
-                                saveState = true
-                            }
-                            // Avoid multiple copies of the same destination when
-                            // reselecting the same item
-                            launchSingleTop = true
-                            // Restore state when reselecting a previously selected item
-                            restoreState = true
-                        }
-                    }
-                )
+                val selected = currentDestination?.route == screen.route
+                CustomBottomNavItem(modifier.weight(1f), selected, screen, navController)
             }
         }
     }
 }
+
+@Composable
+fun CustomBottomNavItem(modifier: Modifier,
+                        selected: Boolean,
+                        screen: BottomBarScreens,
+                        navController: NavController) {
+    val background = if(selected) primaryColor else Color.Transparent
+    val contentColor = if(selected) primaryColor else Color.LightGray
+    val selectedIconColor = if(selected) Color.White else Color.LightGray
+
+    Box(
+        modifier = Modifier
+            .clip(CircleShape)
+            .clickable {
+                navController.navigate(screen.route) {
+                    // Pop up to the start destination of the graph to
+                    // avoid building up a large stack of destinations
+                    // on the back stack as users select items
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    // Avoid multiple copies of the same destination when
+                    // re selecting the same item
+                    launchSingleTop = true
+                    // Restore state when reselecting a previously selected item
+                    restoreState = true
+                }
+            }
+            .padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(imageVector = screen.icon, contentDescription = null,
+                tint = selectedIconColor,
+                modifier = Modifier
+                    .background(background, shape = CircleShape)
+                    .padding(4.dp)
+            )
+            Text(text = screen.title, color = contentColor)
+        }
+    }
+}
+
+
+/*BottomNavigationItem(
+icon = {
+    if (screen.route == Screens.InApp.HomeBottomBar.route)
+        Icon(Icons.Filled.Home, contentDescription = null)
+    if (screen.route == Screens.InApp.ClassBottomBar.route)
+        Icon(Icons.Filled.Clear, contentDescription = null)
+},
+label = { Text(stringResource(screen.resId)) },
+selected = ,
+selectedContentColor = primaryColor,
+unselectedContentColor = Color.LightGray,
+onClick = {
+
+}
+)*/
--- presentation/src/main/java/com/projectprovip/h1eu/giasu/view/navigation/Screens.kt
+++ presentation/src/main/java/com/projectprovip/h1eu/giasu/view/navigation/Screens.kt
@@ -1,6 +1,11 @@
 package com.projectprovip.h1eu.giasu.view.navigation
 
+import android.graphics.drawable.Icon
 import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.projectprovip.h1eu.giasu.R
 
 sealed class Screens(val route: String){
@@ -11,10 +16,10 @@ sealed class Screens(val route: String){
         object ForgetPassword : Screens(route = "forget_password")
     }
     data object InApp : Screens(route = "in_app"){
-        object HomeBottomBar : BottomBarScreens(route = "home", R.string.bottom_home)
+        object HomeBottomBar : BottomBarScreens(route = "home", R.string.bottom_home, icon = Icons.Outlined.Home, title = "Home")
         object ClassDetail : Screens(route = "new_class_detail")
-        object ClassBottomBar : BottomBarScreens(route = "class", R.string.bottom_class)
+        object ClassBottomBar : BottomBarScreens(route = "class", R.string.bottom_class, icon = Icons.Outlined.Warning, title = "Class")
     }
 }
 
-sealed class BottomBarScreens(val route: String, @StringRes val resId: Int)
+sealed class BottomBarScreens(val route: String, @StringRes val resId: Int, val icon: ImageVector, val title: String)
