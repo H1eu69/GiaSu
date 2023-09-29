a0b52dfa030d76e1d46462d05b64217caa018ece
H1eu69 <anrayno200@gmail.com>
2023-09-29 15:20:03 +0700

    add tutor register screen

---
+++ presentation/src/main/java/com/projectprovip/h1eu/giasu/view/screens/in_app/profile/TutorRegister.kt
@@ -0,0 +1,109 @@
+package com.projectprovip.h1eu.giasu.view.screens.in_app.profile
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.projectprovip.h1eu.giasu.ui.composes.EduSmartButton
+import com.projectprovip.h1eu.giasu.ui.composes.InformationTextField
+import com.projectprovip.h1eu.giasu.ui.theme.primaryColor
+
+@Preview
+@Composable
+fun PreviewTutorRegister() {
+    TutorRegisterScreen(rememberNavController())
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TutorRegisterScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            Icons.Rounded.ArrowBack,
+                            null,
+                            tint = Color.White)
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = primaryColor
+                ),
+                title = {
+                    Text(
+                        text = "Tutor Registration",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            color = Color.White,
+                            fontFamily = FontFamily.SansSerif,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+            )
+        }
+    ) {
+        Box(modifier = Modifier
+            .padding(it)
+            .fillMaxSize(),) {
+            LazyColumn(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                contentPadding = PaddingValues( bottom = 100.dp)
+            ) {
+                items(20) {
+                    InformationTextField(remember {
+                        mutableStateOf("profile name")
+                    }, tintIcon = Color.Black,
+                        modifier = Modifier.fillMaxWidth())
+                }
+                item() {
+                    InformationTextField(remember {
+                        mutableStateOf("last field")
+                    }, tintIcon = Color.Black,
+                        false,
+                        modifier = Modifier.fillMaxWidth().
+                        height(150.dp))
+                }
+            }
+            EduSmartButton(
+                onClick = {},
+                text = "Register",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .align(Alignment.BottomCenter))
+        }
+
+
+    }
+}
