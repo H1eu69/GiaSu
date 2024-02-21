567ba013635cec710ce1a1336ae2a8149acec420
H1eu69 <anrayno200@gmail.com>
2024-02-21 16:15:32 +0700

    add search with suggest page, search with result page in home

--- app/src/main/java/com/projectprovip/h1eu/giasu/presentation/home/view/HomeScreen.kt
+++ app/src/main/java/com/projectprovip/h1eu/giasu/presentation/home/view/HomeScreen.kt
@@ -167,6 +167,7 @@ fun BodyContent(
         SearchTextField(onTap = {})
         LazyColumn(
             contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier
                 .fillMaxSize()
                 .background(
@@ -191,7 +192,9 @@ fun BodyContent(
                 when {
                     this.isLoading -> {
                         item {
-                            CircularProgressIndicator()
+                            CircularProgressIndicator(
+                                color = EDSColors.primaryColor
+                            )
                         }
                     }
 
@@ -299,7 +302,6 @@ fun CourseItem(navController: NavController, data: CourseDetail) {
         border = BorderStroke(2.dp, Color.LightGray),
         elevation = CardDefaults.outlinedCardElevation(3.dp),
         modifier = Modifier
-            .padding(8.dp)
             .fillMaxWidth()
             .clip(
                 RoundedCornerShape(10)
@@ -319,7 +321,7 @@ fun CourseItem(navController: NavController, data: CourseDetail) {
             MiddleContent(
                 minutePerSession = data.minutePerSession,
                 sessionPerWeek = data.sessionPerWeek,
-                info = data.description,
+                info = data.subjectName,
                 location = data.address
             )
             BottomContent(
