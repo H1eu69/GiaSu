78db9a2c81a695a7c761328b56ed50b421a0af68
H1eu69 <anrayno200@gmail.com>
2024-03-25 09:22:25 +0700

    api template for update profile

--- app/src/main/java/com/projectprovip/h1eu/giasu/data/profile/api/ProfileApi.kt
+++ app/src/main/java/com/projectprovip/h1eu/giasu/data/profile/api/ProfileApi.kt
@@ -1,12 +1,22 @@
 package com.projectprovip.h1eu.giasu.data.profile.api
 
-import com.projectprovip.h1eu.giasu.data.profile.dto.ProfileDto
+import com.projectprovip.h1eu.giasu.data.profile.dto.profileDto.ProfileDto
+import com.projectprovip.h1eu.giasu.data.profile.dto.updateProfileDto.UpdateProfileDto
+import com.projectprovip.h1eu.giasu.domain.profile.usecase.UpdateProfileParams
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PUT
 
 interface ProfileApi {
     @GET("Profile")
     suspend fun getUserProfile(
         @Header("Authorization") token: String,
     ): ProfileDto
+
+    @PUT("Profile/edit")
+    suspend fun updateProfile(
+        @Header("Authorization") token: String,
+        @Body params: UpdateProfileParams
+    ): UpdateProfileDto
 }
